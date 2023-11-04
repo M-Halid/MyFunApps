40075157a8c4b8bf16b8be1f98539f0130a1d370
Muhammed Halid Kutsal <mhalidkutsal@gmail.con>
2023-11-04 21:48:57 +0100

    Initial Commit

---
+++ MyFunMaps/MyFunMapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFunMapsApp.swift
+//  MyFunMaps
+//
+//  Created by Muhammed Halid Kutsal on 04.11.23.
+//
+
+import SwiftUI
+
+@main
+struct MyFunMapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
